55d208dfb4f01b23ab24932c743a17e4f5efde3c
Nadiia.Zlobynets <euphoria93@gmail.com>
2022-03-26 20:07:49 +0000

    #1 Lesson: changed selectors in css, added variables with colors for css, edited sections (header and main), deleted js files

--- devcloud/assets/js/component/garbage.js
+++
@@ -1,5 +0,0 @@
-const garbage = () => {
-    document.querySelector('.js--year').innerHTML = new Date().getFullYear();
-}
-
-export default garbage;
--- devcloud/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import menu from "./component/menu.js";
-import garbage from './component/garbage.js';
-import slider from "./component/slider.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    menu();
-    garbage();
-    slider();
-});
